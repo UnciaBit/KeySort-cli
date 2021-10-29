1ed5df2e83ff1495141e7108135d673c84bdb280
UnciaBit <ub.github@unciabit.com>
2021-10-30 00:05:54 +0800

    Prototype input splitting into vector

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use std::io;
 use std::fs;
 
 fn main() {
-    
+    //Take inputs and store in Vector separated by spaces
+    let mut input = String::new();
+    println!("Input multiple strings sparated by spaces");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let input: Vec<&str> = input.trim().split_whitespace().collect();
+    let n = input[0];
+    println!("First item: {}", n);
+    println!("Number of inputs: {}", input.len());
 }
